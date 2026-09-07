25731e0801806a1557ece4ef9a507a078190dbc4
PaulDeMaio1 <pfdemaio12@gmail.com>
2026-09-07 17:50:51 -0400

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,38 @@
+
+
+# list of variables about me
+
+first_name = "Paul"
+last_name = "DeMaio"
+age = 28
+city = "Greenville"
+previous_career = "sales rep"
+learn_code = "I want to learn to code so I can start a new career in tech"
+# learn_code will be short for question "why do I want to learn how to code?"
+fun_fact = "I am a triplet"
+
+# formatted table
+
+print("-" * 30)
+
+print("student profile")
+
+print("-" * 30)
+
+print(f"First Name: {first_name}")
+
+print (f"Last Name: {last_name}")
+
+print(f"Age: {age}")
+
+print(f"City: {city}")
+
+print(f"Previous Career: {previous_career}")
+
+print(f"Why I want to learn to code: {learn_code}")
+
+print(f"Fun Fact: {fun_fact}")
+
+# code ends here, place the footer at next line
+print ("-" * 30)
+
